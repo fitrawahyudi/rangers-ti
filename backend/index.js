ee7b8dd54b26bbbf134f1c2a6a48a5dc96b57266
fitrawahyudi <76172044+fitrawahyudi@users.noreply.github.com>
2025-11-06 03:19:55 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import jwt from "jsonwebtoken"
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
